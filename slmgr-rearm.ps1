22b8690b9be41cd59854f3345530bd627f546c3b
stryker2k2 <stryker2k2@msn.com>
2020-02-13 20:22:00 -0600

    Added slmgr-rearm.ps1

---
+++ slmgr-rearm.ps1
@@ -0,0 +1,8 @@
+SLMGR -REARM
+
+set-itemproperty -path "HKCU:Control Panel\Desktop" -name Wallpaper -value C:\Users\Public\Pictures\Sample Pictures\Tulips.jpg
+
+# Set-ExecutionPolicy RemoteSigned
+# Set-ExecutionPolicy Unrestricted
+# Get-ExecutionPolicy
+# Exit
