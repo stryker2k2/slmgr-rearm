e31f5bf4fb79c467397510644c3959aed83f4f08
Jack Ward <stryker2k2@msn.com>
2020-03-18 11:13:43 -0500

    Updated slmgr-rearm to update SkipRearm count and get-itemproperty

--- slmgr-rearm.ps1
+++ slmgr-rearm.ps1
@@ -1,6 +1,12 @@
+set-itemproperty -Path "HKCU:SOFTWARE\Microsoft\Windows NT\CurrentVersion\SoftwareProtectionPlatform" -name SkipRearm -value 1
+
+# get-itemproperty -Path "HKCU:SOFTWARE\Microsoft\Windows NT\CurrentVersion\SoftwareProtectionPlatform" -name SkipRearm | findstr "SkipRearm"
+
 SLMGR -REARM
 
-set-itemproperty -path "HKCU:Control Panel\Desktop" -name Wallpaper -value C:\Users\Public\Pictures\Sample Pictures\Tulips.jpg
+set-itemproperty -path "HKCU:Control Panel\Desktop" -name Wallpaper -value C:\Users\devbox\AppData\Roaming\Microsoft\Windows\Themes\TranscodedWallpaper.jpg
+
+# get-itemproperty -path "HKCU:Control Panel\Desktop" -name Wallpaper | findstr "Wallpaper"
 
 # Set-ExecutionPolicy RemoteSigned
 # Set-ExecutionPolicy Unrestricted
